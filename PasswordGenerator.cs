289e9313e1d7ffba966a184bc75b46636a43ff10
lemmouchimds <80680784+lemmouchimds@users.noreply.github.com>
2022-07-24 16:15:24 +0100

    Add project files.

---
+++ PasswordGenerator.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordGenerator
+{
+    public class PasswordGenerator
+    {
+
+        const int minimumLength = 12;
+        //readonly List<char> alphaList = new List<char>();
+        //readonly List<char> digitList = new List<char>();
+        //readonly List<char> symboleList = new List<char>();
+
+        Dictionary<PwModes, List<char>> dictionary = new Dictionary<PwModes, List<char>>();
+
+        public PasswordGenerator()
+        {
+            GenerateLists();
+        }
+        public string Generate(int length = minimumLength)
+        {
+            if (length < minimumLength)
+                return null;
+
+            Random random = new Random();
+            StringBuilder result = new StringBuilder();
+
+            for (int i = 0; i < length; i++)
+            {
+                int rand = random.Next('!', 'z');
+                result.Append((char)rand);
+            }
+
+
+            return result.ToString();
+        }
+
+        void GenerateLists()
+        {
+            var alphaList = new List<char>();
+            var digitList = new List<char>();
+            var symboleList = new List<char>();
+
+            for (char i = '!'; i <= 'z'; i++)
+            {
+                if (char.IsLetter((char)i))
+                {
+                    alphaList.Add((char)i);
+                }
+                else if (char.IsDigit((char)i))
+                {
+                    digitList.Add((char)i);
+                }
+                else
+                {
+                    symboleList.Add((char)i);
+                }
+            }
+
+            dictionary.Add(PwModes.Alpha, alphaList);
+            dictionary.Add(PwModes.Digits, digitList);
+            dictionary.Add(PwModes.Symbols, symboleList);
+        }
+
+
+        public string GenerateFromLists(int length, PwModes pwMode)
+        {
+            if (pwMode == PwModes.None)
+                return null;
+                       
+
+            StringBuilder result = new StringBuilder();
+
+            List<char> MyList = new List<char>();
+
+            foreach (var element in dictionary)
+            {
+                if((pwMode & element.Key) == element.Key)
+                    MyList.AddRange(element.Value);
+            }
+
+
+
+            /*
+
+            if ((pwMode & PwModes.Alpha) == PwModes.Alpha)
+            {
+              //  MyList.AddRange(alphaList);
+
+                foreach (var v in alphaList)
+                {
+                    MyList.Add(v);
+                }
+            }
+
+            if ((pwMode & PwModes.Digits) == PwModes.Digits)
+            {
+                foreach (var v in digitList)
+                {
+                    MyList.Add(v);
+                }
+            }
+
+            if ((pwMode & PwModes.Symbols) == PwModes.Symbols)
+            {
+                foreach (var v in symboleList)
+                {
+                    MyList.Add(v);
+                }
+            }
+            */
+            Random rand = new Random();
+            int max = MyList.Count;
+
+            for (int i = 0; i < length; i++)
+            {
+                int pwInt = rand.Next(max);
+                char pwChar = MyList[pwInt];
+                result = result.Append(pwChar);
+            }
+
+            return result.ToString();
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string output = string.Empty;
+
+            Console.WriteLine("do you want a fix string in the begginig ?");
+            var respond = Console.ReadLine().ToLower();
+
+            if (string.Equals(respond, "yes"))
+            {
+                Console.WriteLine("give me the prefix : ");
+                var prefix = Console.ReadLine();
+                output += prefix;
+            }
+
+            PasswordGenerator a = new PasswordGenerator();
+            output += a.GenerateFromLists(13, PwModes.Digits | PwModes.Alpha | PwModes.Symbols);
+            Console.WriteLine("this is your password : " + output);
+        }
+
+    }
+
+    [Flags]
+    public enum PwModes
+    {
+        None = 0x00,
+        Alpha = 0x01,
+        Digits = 0x02,
+        Symbols = 0x04
+    }
+
+}
